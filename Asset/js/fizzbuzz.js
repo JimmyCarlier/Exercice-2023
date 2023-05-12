aac7a881310505b34225604dfe79f4534fc5c4de
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-05-12 15:39:00 +0200

    add force brut

--- Asset/js/fizzbuzz.js
+++ Asset/js/fizzbuzz.js
@@ -333,7 +333,6 @@
 //   }
 // }
 
-
 // FORCE BRUT
 
 // let digicode = [];
@@ -354,12 +353,65 @@
 //     if(digicode[i] == j){
 //       crack.push(j);
 //       break;
-//     } 
+//     }
 //   }
-//   console.log("crack " + crack + " digicode " + digicode) 
+//   console.log("crack " + crack + " digicode " + digicode)
 // }
 
+// let digicode = [];
+// let crack = []
 
+// for (let i = 0; i < 3; i++) {
+//   digicode.push(Math.floor(Math.random()*10));
+// }
 
+// for(let i=0; i < 3; i++){
+//   for(let j = 0; j <=9 ; j++){
+//     if(digicode[i] == j){
+//       crack.push(j);
+//       break;
+//     }
+//   }
+//   console.log("crack " + crack + " digicode " + digicode)
+// }
 
+// if while
+// let digicode = [];
+// let crack = [];
 
+// for (let i = 0; i < 3; i++) {
+//   digicode.push(Math.floor(Math.random() * 10));
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   let j = 0;
+//   while (j <= 9) {
+//     if (digicode[i] == j) {
+//       crack.push(j);
+//       break;
+//     }
+//     j++;
+//   }
+//   i++;
+//   console.log("crack " + crack + " digicode " + digicode);
+// }
+
+let digicode = [];
+let crack = [];
+
+for (let i = 0; i < 3; i++) {
+  digicode.push(Math.floor(Math.random() * 10));
+}
+
+let i = 0;
+while (i < 3) {
+  for (let j = 0; j <= 9; j++) {
+    if (digicode[i] == j) {
+      crack.push(j);
+      break;
+    }
+  }
+  i++;
+  console.log("crack " + crack + " digicode " + digicode);
+}

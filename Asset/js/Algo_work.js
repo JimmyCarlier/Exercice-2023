7c82b0d5d4579ab8bfc95e4cc8ba50cb4cc49317
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-05-19 12:17:19 +0200

    Modification du nom de dossier

--- Asset/js/Algo_work.js
+++ Asset/js/Algo_work.js
@@ -495,7 +495,6 @@
 //     console.log(line); // Affiche la ligne dans la console
 // }
 
-
 // let code = []
 // let crack = []
 
@@ -512,26 +511,33 @@
 //     console.log("crack "+crack+" code "+code)
 // }
 
-let size = 20;
-let char = "A";
-let tableRandom = [];
-let blank;
-let tableVille ;
+// Ville
+// let size = 20;
+// let char = "A";
+// let tableRandom = [];
+// let blank;
+// let tableVille ;
 
-for (i = 0; i < size; i++) {
-    tableRandom[i] = Math.floor(Math.random() * 10);
-}
+// for (i = 0; i < size; i++) {
+//     tableRandom[i] = Math.floor(Math.random() * 10);
+// }
 
-for (i = 0; i < size; i++) {
-    tableVille="";
-    for (j = 0; j <= size; j++) {
-        blank = (size - tableRandom[j]);
-        if(i<blank){
-            tableVille+= " "
-        }
-        if(i>=blank){
-            tableVille+= "A"
-        }
-    }
-    console.log(tableVille)
-}
+// for (i = 0; i < size; i++) {
+//     tableVille="";
+//     for (j = 0; j <= size; j++) {
+//         blank = (size - tableRandom[j]);
+//         if(i<blank){
+//             tableVille+= " "
+//         }
+//         if(i>=blank){
+//             tableVille+= "A"
+//         }
+//     }
+//     console.log(tableVille)
+// }
+
+// let table = [[1, "FizzBuzz"], [2, "Crackcode"], [3, "Ville"], [4, "Carré de X"], [5, "X vide"], [6, "Checkermate"], [7, "XOXO"], [8, "FizzBuzzTapioca"]];
+// let hasard = Math.floor(Math.random() * table.length);
+// let exerciceAleatoire = table[hasard][1];
+
+// console.log(exerciceAleatoire);
